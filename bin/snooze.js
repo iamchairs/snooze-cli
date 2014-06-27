4e0a535d3e7586f3e5d3d0bb0d47338b2727f4d1
Micah Williamson <micah.williamson@firemon.com>
2014-06-27 10:04:22 -0500

    refactored to use commander

--- bin/snooze.js
+++ bin/snooze.js
@@ -4,28 +4,85 @@ var fs = require('fs');
 var colors = require('colors');
 var snooze = require(process.cwd() + '/node_modules/snooze');
 var _ = require('lodash');
-var modname;
+var program = require('commander');
 
-var _fatal = function(str) {
-	console.log(str.red);
-	process.exit(1);
+var _conf = {};
+var _params = {};
+
+if(snooze.getConfig().main !== undefined) {
+	var main = snooze.getConfig().main;
+	if(fs.existsSync(process.cwd() + '/' + main)) {
+		var hand = console.log;
+		console.log = function(){};
+
+		require(process.cwd() + '/' + main);
+
+		console.log = hand;
+	} else {
+		snooze.fatal('Fatal Error: ' + main + ' doesn\'t exist');
+	}
+
+} else {
+	snooze.fatal('Fatal Error: No main defined in snooze.json');
+}
+
+var _normalizeOptions = function(options) {
+	var normalizedObj = {};
+	var parent = options;
+	while(parent !== undefined) {
+		var _options = parent.options;
+		for(var i = 0; i < _options.length; i++) {
+			var _option = _options[i];
+			var re = /[<\[](.*)[>\]]/;
+			var matches = re.exec(_option.flags);
+			if(matches !== null) {
+				var prop = matches[1];
+				normalizedObj[prop] = parent[prop];
+			}
+		}
+
+		parent = parent.parent;
+	}
+
+	return normalizedObj;
 };
 
-var printRoutes = function(routes, options) {
+var getRealModule = function(module) {
+	if(module === undefined) {
+		if(snooze.getConfig().name !== undefined) {
+			return snooze.getConfig().name;
+		} else {
+			snooze.fatal('Fatal Error: no module defined. you can also set "name" in snooze.json to avoid naming it in snooze-cli');
+		}
+	} else {
+		if(snooze.moduleExists(module) === true) {
+			return module;
+		} else {
+			snooze.fatal('Fatal Error: module ' + module + ' doesn\'t exist');
+		}
+	}
+};
+
+var printRoutes = function(options) {
+	var prepend = '';
+
+	var module = getRealModule(options.module);
+	if(options.routes) {
+		var routes = options.routes;
+	} else {
+		var routes = snooze.module(module).getRoutes(options.type);
+	}
+
+	if(options.prepend) {
+		prepend = options.prepend;
+	}
+
 	routes = _.sortBy(routes, function(route) {
 		return route.getMethod();
 	});
 
 	_.each(routes, function(route) {
-		var out = route.getMethod().toUpperCase() + ' => ' + route.getPath();
-
-		if(options === undefined) {
-			options = {};
-		}
-
-		if(options.prepend) {
-			out = options.prepend + out;
-		}
+		var out = prepend + route.getMethod().toUpperCase() + ' => ' + route.getPath();
 
 		switch(route.getMethod()) {
 			case 'get':
@@ -47,14 +104,17 @@ var printRoutes = function(routes, options) {
 	});
 };
 
-var printControllers = function(controllers) {
+var printControllers = function(options) {
+	var module = getRealModule(options.module);
+	var controllers = snooze.module(module).getControllers();
+
 	controllers = _.sortBy(controllers, function(ctrl) {
 		return ctrl.getName();
 	});
 
-	var routes = snooze.module(modname).getRoutes();
-	var services = snooze.module(modname).getServices();
-	var dtos = snooze.module(modname).getDTOs();
+	var routes = snooze.module(module).getRoutes();
+	var services = snooze.module(module).getServices();
+	var dtos = snooze.module(module).getDTOs();
 
 	_.each(controllers, function(ctrl) {
 		console.log(ctrl.getName().green);
@@ -71,7 +131,7 @@ var printControllers = function(controllers) {
 
 		if(ctrlRoutes.length > 0) {
 			console.log('\t@Routes');
-			printRoutes(ctrlRoutes, {prepend: '\t'});
+			printRoutes({routes: ctrlRoutes, prepend: '\t'});
 		}
 
 		if(ctrlServices.length > 0) {
@@ -90,13 +150,16 @@ var printControllers = function(controllers) {
 	});
 };
 
-var printServices = function(services) {
+var printServices = function(options) {
+	var module = getRealModule(options.module);
+	var services = snooze.module(module).getDTOs();
+
 	services = _.sortBy(services, function(srv) {
 		return srv.getName();
 	});
 
-	var services = snooze.module(modname).getServices();
-	var dtos = snooze.module(modname).getDTOs();
+	var services = snooze.module(module).getServices();
+	var dtos = snooze.module(module).getDTOs();
 
 	_.each(services, function(srv) {
 		console.log(srv.getName().green);
@@ -120,13 +183,16 @@ var printServices = function(services) {
 	});
 };
 
-var printValidators = function(validators) {
+var printValidators = function(options) {
+	var module = getRealModule(options.module);
+	var validators = snooze.module(module).getValidators();
+
 	validators = _.sortBy(validators, function(vd) {
 		return vd.getName();
 	});
 
-	var services = snooze.module(modname).getServices();
-	var dtos = snooze.module(modname).getDTOs();
+	var services = snooze.module(module).getServices();
+	var dtos = snooze.module(module).getDTOs();
 
 	_.each(validators, function(vd) {
 		console.log(vd.getName().green);
@@ -150,7 +216,10 @@ var printValidators = function(validators) {
 	});
 };
 
-var printDTOs = function(dtos) {
+var printDTOs = function(options) {
+	var module = getRealModule(options.module);
+	var dtos = snooze.module(module).getDTOs();
+
 	_.each(dtos, function(dto) {
 		console.log(dto.getName().green);
 
@@ -196,258 +265,97 @@ var printDTOs = function(dtos) {
 	});
 };
 
-var methods = {
-	'controllers': function() {
-		printControllers(snooze.module(modname).getControllers());
-	},
-	'services': function() {
-		printServices(snooze.module(modname).getServices());
-	},
-	'validators': function() {
-		printValidators(snooze.module(modname).getValidators());
-	},
-	'dtos': function() {
-		printDTOs(snooze.module(modname).getDTOs());
-	},
-	'routes.get': function() {
-		printRoutes(snooze.module(modname).getRoutes('get'));
-	},
-	'routes.post': function() {
-		printRoutes(snooze.module(modname).getRoutes('post'));
-	},
-	'routes.put': function() {
-		printRoutes(snooze.module(modname).getRoutes('put'));
-	},
-	'routes.delete': function() {
-		printRoutes(snooze.module(modname).getRoutes('delete'));
-	},
-	'routes.resource': function() {
-		printRoutes(snooze.module(modname).getRoutes('resource'));
-	},
-	'routes': function() {
-		printRoutes(snooze.module(modname).getRoutes());
-	},
-	'help': function() {
-		console.log('snooze Help Page'.red);
-		console.log();
-		console.log('=== Commands ==='.green);
-		console.log();
+var generateAPI = function(options) {
+	var module = getRealModule(options.module);
 
-		for(var flag in flags) {
-			var flagData = flags[flag];
-			var parameters = flagData.methods;
-			var paramStr = '';
+	if(fs.existsSync(process.cwd() + '/api') === false) {
+		fs.mkdirSync(process.cwd() + '/api');
+	}
 
-			var keys = _.without(Object.keys(parameters), $end);
-			if(parameters.$end !== undefined) {
-				delete keys.$end;
-			}
+	var api = {
+		module: module,
+		modules: [],
+		routes: [],
+		services: [],
+		controllers: [],
+		validators: [],
+		dtos: []
+	};
 
-			if(keys.length > 0) {
-				paramStr = ' [' + keys.join(', ') + ']';
-			}
-
-			console.log(('\tsnooze [module] ' + flag + paramStr).yellow);
-			//console.log(('\tAliases: ' + flagData.aliases.join(', ')).yellow);
-			console.log('\t-' + flagData.description);
-			console.log();
-		}
-	},
-	'api': function() {
-		if(fs.existsSync(process.cwd() + '/api') === false) {
-			fs.mkdirSync(process.cwd() + '/api');
-		}
-
-		var api = {
-			module: modname,
-			modules: [],
-			routes: [],
-			services: [],
-			controllers: [],
-			validators: [],
-			dtos: []
+	var routes = snooze.module(module).getRoutes();
+	_.each(routes, function(route) {
+		var rt = {
+			method: route.getMethod(),
+			path: route.getPath(),
+			response: route.getResponse(),
+			request: route.getRequest()
 		};
 
-		var routes = snooze.module(modname).getRoutes();
-		_.each(routes, function(route) {
-			var rt = {
-				method: route.getMethod(),
-				path: route.getPath(),
-				response: route.getResponse(),
-				request: route.getRequest()
-			};
+		api.routes.push(rt);
+	});
 
-			api.routes.push(rt);
-		});
+	var dtos = snooze.module(module).getDTOs();
+	_.each(dtos, function(dto) {
+		var dto = {
+			name: dto.getName(),
+			properties: dto.getProperties(),
+			strict: dto.isStrict()
+		};
 
-		var dtos = snooze.module(modname).getDTOs();
-		_.each(dtos, function(dto) {
-			var dto = {
-				name: dto.getName(),
-				properties: dto.getProperties(),
-				strict: dto.isStrict()
-			};
+		api.dtos.push(dto);
+	});
 
-			api.dtos.push(dto);
-		});
-
-		fs.writeFileSync(process.cwd() + '/api/' + modname + '.api.json', JSON.stringify(api, null, 2));
-	},
-	'init': function() {
-		_initDirectories();
-		_initMain();
-	},
-	'init.helloworld': function() {
-		_initDirectories();
-		_initHelloWorld();
-	}
+	fs.writeFileSync(process.cwd() + '/api/' + module + '.api.json', JSON.stringify(api, null, 2));
 };
 
-$end = '$end';
-var flags = {
-	'help': {
-		methods: {
-			$end: 'help'
-		},
-		aliases: ['-h', '--help'],
-		description: 'Display the command line help page.'
-	},
-	'-r': {
-		methods: {
-			'get': 'routes.get',
-			'post': 'routes.post',
-			'put':  'routes.put',
-			'delete': 'routes.delete',
-			'resource': 'routes.resource',
-			$end: 'routes'
-		},
-		aliases: ['--routes'],
-		description: 'Lists the routes in the snooze application.'
-	},
-	'-c': {
-		methods: {
-			$end: 'controllers'
-		},
-		description: 'Lists the controllers in the snooze application, their injectables, and routes that point to them.',
-		aliases: ['--controllers']
-	},
-	'-s': {
-		methods: {
-			$end: 'services'
-		},
-		aliases: ['--services'],
-		description: 'Lists the services in the snooze application and their injectables.'
-	},
-	'-d': {
-		methods: {
-			$end: 'dtos'
-		},
-		aliases: ['--dtos'],
-		description: 'Lists the dtos in the snooze application, their injectables, and properties.'
-	},
-	'-v': {
-		methods: {
-			$end: 'validators'
-		},
-		aliases: ['--validators'],
-		description: 'Lists the validators in the snooze application and their injectables.'
-	},
-	'api': {
-		methods: {
-			$end: 'api'
-		},
-		description: 'Generates a [module].api.json API File in the api directory.'
-	}/*,
-	'init': {
-		methods: {
-			HelloWorld: 'init.helloworld',
-			$end: 'init'
-		},
-		description: 'Initializes a new flint module with empty directories and a main.js file.'
-	}*/
-};
+program
+	.version('0.0.3')
+	.option('-m, --module <module>', 'Set the module');
 
-var runFlag = function(flag) {
-	if(flags(flag)) {
+program
+	.command('routes')
+	.description('Lists the routes in the snooze application.')
+	.option('-t, --type <type>', 'GET, POST, PUT, DELETE, RESOURCE')
+	.action(function(options) {
+		printRoutes(_normalizeOptions(options));
+	});
 
-	}
-};
+program
+	.command('controllers')
+	.description('Lists the controllers in the snooze application, their injectables, and routes that point to them.')
+	.action(function(options) {
+		printControllers(_normalizeOptions(options));
+	});
 
-var flagHasMethod = function(flag, method) {
-	if(flags[flag].methods[method] !== undefined) {
-		return true;
-	};
-	return false;
-};
+program
+	.command('services')
+	.description('Lists the services in the snooze application and their injectables.')
+	.action(function(options) {
+		printServices(_normalizeOptions(options));
+	});
 
-var runFlagMethod = function(flag, method) {
-	var method = flags[flag].methods[method];
-	methods[method]();
-};
+program
+	.command('dtos')
+	.description('Lists the dtos in the snooze application, their injectables, and properties.')
+	.action(function(options) {
+		printDTOs(_normalizeOptions(options));
+	});
 
-if(fs.existsSync('./main.js')) {
-	var modname = process.argv[2];
-	var args = process.argv.splice(3);
+program
+	.command('validators')
+	.description('Lists the validators in the snooze application and their injectables.')
+	.action(function(options) {
+		printValidators(_normalizeOptions(options));
+	});
 
-	var hand = console.log;
-	console.log = function() {};
+program
+	.command('api')
+	.description('Generates a [module].api.json API File in the api directory.')
+	.action(function(options) {
+		generateAPI(_normalizeOptions(options));
+	});
 
-	require(process.cwd() + '/main.js');
+program
+	.parse(process.argv);
 
-	if(snooze.moduleExists(modname)) {
-		console.log = hand;
-
-		if(args.length < 1) {
-			args.push('help');
-		}
-
-		// running params
-
-		for(var i = 0; i < args.length; i++) {
-			var argStack = [];
-			var flag = args[i];
-			if(flags[flag]) {
-				argStack.push(flag);
-				var argStackReached = false;
-				var k = i+1;
-
-				if(args.length > k) {
-					var stackVal = args[k];
-					if(flagHasMethod(flag, stackVal)) {
-						runFlagMethod(flag, stackVal);
-						i++;
-					} else {
-						if(flagHasMethod(flag, $end)) {
-							runFlagMethod(flag, $end);
-						} else {
-							_fatal('Flag ' + flag + ' doesn\'t support uknown parameter ' + stackVal);
-						}
-					}
-				} else {
-					if(flagHasMethod(flag, $end)) {
-						runFlagMethod(flag, $end);
-					} else {
-						_fatal('Flag ' + flag + ' expects additional parameters.');
-					}
-				}
-
-			} else {
-				_fatal('Unknown flag : ' + args[i]);
-			}
-		}
-
-		// End the server
-
-		process.exit(0);
-
-	} else {
-		console.log = hand;
-		if(modname === undefined) {
-			_fatal('Use: snooze [module] help');
-		} else {
-			_fatal('Module ' + modname + ' doesn\'t exist');
-		}
-	}
-} else {
-	_fatal('Unable to find server\'s main.js file in the current directory.');
-}
+process.exit(0);
